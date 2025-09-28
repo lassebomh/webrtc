159205ecaf027e4ab01119e2bff2bc200e27aca8
Lasse H. Bomholt <lasse@bomh.net>
2025-09-28 22:06:29 +0200

    stuff

--- docs/main.js
+++ docs/main.js
@@ -38,49 +38,68 @@ signal.addEventListener("message", async (e) => {
       send({ type: "ice-candidate", candidate }, from);
     });
 
-    const channel = peer.createDataChannel("default");
-    channels.set(from, channel);
-
-    channel.addEventListener("close", (e) => {
-      peer.close();
-      channel.close();
-      peers.delete(from);
-      channels.delete(from);
-    });
-    channel.addEventListener("message", (e) => {
-      console.log(e.data);
-    });
-
     peers.set(from, peer);
   }
 
   switch (msg.type) {
-    case "init":
+    case "init": {
+      const channel = peer.createDataChannel("default");
+      channels.set(from, channel);
+
+      channel.addEventListener("message", (e) => {
+        console.log(e.data);
+      });
+
+      channel.addEventListener("close", () => {
+        peer.close();
+        channel.close();
+        peers.delete(from);
+        channels.delete(from);
+      });
+
       const offer = await peer.createOffer();
       await peer.setLocalDescription(offer);
       send({ type: "offer", sdp: offer.sdp }, from);
       break;
+    }
 
-    case "offer":
+    case "offer": {
+      peer.ondatachannel = (event) => {
+        const channel = event.channel;
+        channels.set(from, channel);
+
+        channel.addEventListener("message", (e) => {
+          console.log(e.data);
+        });
+
+        channel.addEventListener("close", () => {
+          peer.close();
+          channel.close();
+          peers.delete(from);
+          channels.delete(from);
+        });
+      };
+
       await peer.setRemoteDescription({ type: "offer", sdp: msg.sdp });
       const answer = await peer.createAnswer();
       await peer.setLocalDescription(answer);
       send({ type: "answer", sdp: answer.sdp }, from);
       break;
+    }
 
-    case "answer":
+    case "answer": {
       await peer.setRemoteDescription({ type: "answer", sdp: msg.sdp });
       break;
+    }
 
-    case "ice-candidate":
+    case "ice-candidate": {
       await peer.addIceCandidate(msg.candidate);
       break;
+    }
   }
 });
 
 function sendJSON() {
-  console.log("hello");
-
   for (const [k, v] of channels) {
     v.send("hello " + k);
   }
